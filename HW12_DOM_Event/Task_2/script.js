84750c80373bcfa2d58759bbcf794aecfeecc8fa
Tsobenko Ievgen <kalistratus82@gmail.com>
2020-09-13 19:29:00 +0300

    Revision HW12

--- HW12_DOM_Event/Task_2/script.js
+++ HW12_DOM_Event/Task_2/script.js
@@ -4,17 +4,15 @@ function changeColor(e) {
   const square = e.target;
   if (!square.classList.contains("square")) return;
 
-  let colors = ["blue", "green", "yellow"];
+  const colors = ["blue", "green", "yellow"];
+  const indexOfColor = colors.indexOf(square.classList[1]);
 
-  if (square.classList.contains(colors[0])) {
-    square.classList.add(colors[1]);
-    square.classList.remove(colors[0]);
-  } else if (square.classList.contains(colors[1])) {
-    square.classList.add(colors[2]);
-    square.classList.remove(colors[1]);
-  } else {
+  if (indexOfColor === colors.length - 1) {
+    square.classList.remove(colors[colors.length - 1]);
     square.classList.add(colors[0]);
-    square.classList.remove(colors[2]);
+  } else {
+    square.classList.remove(colors[indexOfColor]);
+    square.classList.add(colors[indexOfColor + 1]);
   }
 }
 
