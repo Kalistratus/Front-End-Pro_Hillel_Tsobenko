ef4dc2295f5a3433160dae93bcd08b31c260d300
Tsobenko Ievgen <kalistratus82@gmail.com>
2020-09-27 17:50:24 +0300

    Fix homework #14

--- HW14_Prototype/script.js
+++ HW14_Prototype/script.js
@@ -8,38 +8,22 @@ function Unit(type, health, maxHealth, maxDistance) {
 
 // функция принимает один параметр - дистанцию, которую нужно пройти юниту. Функция должна проверить способен ли юнит пройти указанную дистанцию.
 Unit.prototype.isReadyToMove = function (distance) {
-  if (distance >= this.maxDistance) {
-    console.log(`Юнит может пройти дистанцию = ${distance}`);
-  } else {
-    console.log(`Юнит не может пройти дистанцию = ${distance}`);
-  }
+  return distance <= this.maxDistance ? true : false;
 }
 
 // функция, проверяющая способен ли юнит сражаться. Юнит способен сражаться если его текущее здоровье составляет хотя бы половину от максимально возможного.
 Unit.prototype.isReadyToFight = function () {
-  if (this.health >= this.maxHealth / 2) {
-    console.log(`Юнит способен сражаться: здоровье = ${this.health / this.maxHealth * 100}%`);
-  } else {
-    console.log(`Юнит не способен сражаться: здоровье = ${this.health / this.maxHealth * 100}%`);
-  }
+  return (this.health >= this.maxHealth / 2) ? true : false;
 }
 
 // функция, которая проверяет был ли ранен юнит. Если был, то восстанавливает здоровье до максимального.
 Unit.prototype.restore = function () {
-  const oldHealth = this.health;
-
-  if (this.health < this.maxHealth) {
-    this.health = this.maxHealth;
-    console.log(`Old health: ${oldHealth}; new health: ${this.maxHealth}`);
-  }
-  console.log(this);
+  return this.health = this.health < this.maxHealth ? this.maxHealth : this.health;
 }
 
 // функция возвращает копию юнита.
 Unit.prototype.clone = function () {
-  const unit = new Unit(this.type, this.health, this.maxHealth, this.maxDistance);
-  console.log(unit);
-  return unit;
+  return new Unit(this.type, this.health, this.maxHealth, this.maxDistance);
 }
 
 
@@ -53,62 +37,50 @@ function Army(defaultUnits) {
 // функция принимает один параметр - дистанцию, которую нужно проти армии. Армия способна пройти дистанцию если все юниты способны пройти дистанцию.
 Army.prototype.isReadyToMove = function (distance) {
   const arr = [];
+  for (const unit of this.units) arr.push(unit.isReadyToMove(distance));
 
-  this.units.forEach(element => arr.push(element.maxDistance));
-  const isDistance = arr.every(element => distance <= element);
-
-  if (isDistance) console.log(`Армия может пройти дистанцию = ${distance}`);
-  else console.log(`Армия не может пройти дистанцию = ${distance}`);
+  return arr.every(element => element === true);
 }
 
 // функция, проверяющая способна ли армия сражаться. Армия способна сражаться если все юниты способны сражаться.
 Army.prototype.isReadyToFight = function () {
   const arr = [];
+  for (const unit of this.units) arr.push(unit.isReadyToFight());
 
-  this.units.forEach(element => arr.push(element.health));
-  const isHealth = arr.every(element => element === 100);
-
-  if (isHealth) console.log("Армия может сражаться");
-  else console.log("Армия не может сражаться");
+  return arr.every(element => element === true);
 }
 
 // проверка здоровья всех юнитов. Функция восстанавливает здоровье всех раненых юнитов.
 Army.prototype.restore = function () {
-
-  for (const element of this.units) {
-    if (element.health < element.maxHealth) element.health = element.maxHealth;
-  }
-  console.log(army);
+  for (const unit of this.units) unit.restore();
 }
 
 // функция принимает один параметр - дистанцию, которую нужно пройти армии. Возвращает массив юнитов, которые способны пройти эту дистанцию.
 Army.prototype.getReadyToMoveUnits = function (distance) {
   const arr = [];
 
-  for (const element of this.units) {
-    if (distance <= element.maxDistance) arr.push(element);
+  for (const unit of this.units) {
+    if (unit.isReadyToMove(distance)) arr.push(unit);
   }
-
-  console.log(arr);
+  return arr;
 }
 
 // функция принимает массив юнитов как параметр. Добавляет всех юнитов из этого массива к массиву собственных юнитов.
 Army.prototype.combineUnits = function (newUnits) {
-  this.units = this.units.concat(newUnits);
+  return this.units = this.units.concat(newUnits);
 }
 
 // принимает порядковый номер юнита, которого нужно скопировать. Возвращает копию юнита.
 Army.prototype.cloneUnit = function (n) {
-  const newUser = this.units[n - 1];
-  console.log(newUser);
-  return newUser;
+  const unit = this.units[n - 1];
+  return unit !== undefined ? unit.clone() : undefined;
 }
 
 
 // Массив дефолтных юнитов
 const defaultUnits = [
   new Unit("soldier", 70, 100, 5000),
-  new Unit("soldier", 70, 100, 4000),
+  new Unit("soldier", 60, 100, 4000),
   new Unit("captain", 85, 100, 6000),
   new Unit("general", 100, 100, 11000)
 ]
